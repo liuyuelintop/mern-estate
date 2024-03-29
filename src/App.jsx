c79bcfebae201ad47db73a3814998a83daa91d2f
liuyuelintop <liuyuelintop@gmail.com>
2024-03-29 20:48:04 +1100

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import SignIn from "./pages/SignIn"
 import Profile from "./pages/Profile"
 import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
+import Header from "./components/Header"
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route />
         <Route path="/" element={<Home />} />
