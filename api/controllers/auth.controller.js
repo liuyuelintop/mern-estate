c336f43d2840f809ed764b9dbc517cc0df9f92ee
liuyuelintop <liuyuelintop@gmail.com>
2024-03-30 23:46:02 +1100

    create user update api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,52 +1,48 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-    });
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
-        res.status(201).json("User created successfully");
+        res.status(201).json('User created successfully!');
     } catch (error) {
         next(error);
     }
-}
+};
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404, 'User not found'));
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-        const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
-        res.cookie('access_token', token, { httpOnly: true })
+        res
+            .cookie('access_token', token, { httpOnly: true })
             .status(200)
             .json(rest);
     } catch (error) {
         next(error);
     }
-}
-
-// Utility function to generate JWT token and send the response
-const createTokenAndRespond = async (user, res) => {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const { password, ...rest } = user._doc; // Exclude the password from the response
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
 };
-
 
 export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email: req.body.email });
         if (user) {
-            await createTokenAndRespond(user, res);
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = user._doc;
+            res
+                .cookie('access_token', token, { httpOnly: true })
+                .status(200)
+                .json(rest);
         } else {
             const generatedPassword =
                 Math.random().toString(36).slice(-8) +
@@ -61,8 +57,22 @@ export const google = async (req, res, next) => {
                 avatar: req.body.photo,
             });
             await newUser.save();
-            await createTokenAndRespond(newUser, res);
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = newUser._doc;
+            res
+                .cookie('access_token', token, { httpOnly: true })
+                .status(200)
+                .json(rest);
         }
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signOut = async (req, res, next) => {
+    try {
+        res.clearCookie('access_token');
+        res.status(200).json('User has been logged out!');
     } catch (error) {
         next(error);
     }
