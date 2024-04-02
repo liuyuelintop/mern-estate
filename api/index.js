bf2c33d02da2b25750bd6877a12ca5a96a9d2e04
liuyuelintop <liuyuelintop@gmail.com>
2024-04-02 17:08:03 +1100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 mongoose
@@ -16,9 +17,17 @@ mongoose
         console.log(err);
     })
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
